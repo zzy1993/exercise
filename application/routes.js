4f153ed203268d32f1ae74fbd3b3c6fa936e21aa
zzy1993 <zzy1993@163.com>
2016-11-17 12:41:10 -0800

    import pageCtrl only as a test

--- application/routes.js
+++ application/routes.js
@@ -1,20 +1,20 @@
 /**
- * define the routes
+ * define module to set routes
  */
 var express = require('express');
 
 module.exports = function (app) {
-
+p
     // import controller modules
-    var images = require('./controllers/imageController.js');
-    var pages = require('./controllers/pagesController.js');
-    var comments = require('./controllers/commentsController.js');
+    // var images = require('./controllers/imageController.js');
+    var pages = require('./controllers/pageController.js');
+    // var comments = require('./controllers/commentController.js');
 
     // define request route to methods in controllers
-    app.get('/images', images.getImages);
-    app.get('/image', images.getImage);
+    // app.get('/images', images.getImages);
+    // app.get('/image', images.getImage);
     app.get('/page', pages.getPage);
-    app.get('/comments/get', comments.getComment);
-    app.post('/comments/add', comments.addComment);
+    // app.get('/comments/get', comments.getComment);
+    // app.post('/comments/add', comments.addComment);
 };
 
