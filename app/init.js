385f2d9a051691746574be458c68579b06e4f96f
zzy1993 <zzy1993@163.com>
2016-11-19 19:38:11 -0500

    add test image files
    add some ancient maps as test case.
    they are stored in project folder.
    better to store in db.

--- app/init.js
+++ app/init.js
@@ -15,3 +15,18 @@ function addImage(title, filename) {
     });
 }
 
+image.remove().exec(function () {
+    addImage('Boston_1728_Burgis','Boston_1728_Burgis.jpg');
+    addImage('Boston_1769_Price_LC','Boston_1769_Price_LC.jpg');
+    addImage('Boston_1775_deCosta','Boston_1775_deCosta.jpg');
+    addImage('Boston_1775_Harbor_Wheeler','Boston_1775_Harbor_Wheeler.jpg');
+    addImage('Boston_1775_Page','Boston_1775_Page.jpg');
+    addImage('Boston_1776_unknown','Boston_1776_unknown.jpg');
+    addImage('Boston_1777_Page','Boston_1777_Page.jpg');
+    addImage('Boston_1805_Carelton','Boston_1805_Carelton.jpg');
+    addImage('Boston_1814_Hales','Boston_1814_Hales.jpg');
+    addImage('Boston_1846_Smith','Boston_1846_Smith.jpg');
+    addImage('Boston_1852_Shields','Boston_1852_Shields.jpg');
+    addImage('Boston_1874_Hopkins','Boston_1874_Hopkins.jpg');
+    addImage('Boston_1875_Beers','Boston_1875_Beers.jpg');
+})
