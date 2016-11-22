dd8c93a4e8f537d090f7c9c15c4340548e755d88
zzy1993 <zzy1993@163.com>
2016-11-22 01:42:27 -0500

    add page controller

--- app/static/js/imagesApp.js
+++ app/static/js/imagesApp.js
@@ -1,26 +1,38 @@
 /**
- *
+ * 
  */
 
 var app = angular.module('myApp', []);
 
 app.controller('imageController', ['$scope', '$http', function($scope, $http){
+    // /image?imageId=5830f1e57467e640eb4da00f
+    $scope.setImage = function(imageId){
+        // use 'params' here
+        $http.get('/image', {params: {imageId: imageId}})
+            .then(function successCallback(res) {
+                $scope.image = res.data;
+            }, function errorCallback(res) {
+                $scope.image = {};
+            });
+    };
+    // /images
     $http.get('/images')
-        // /images
         .then(function successCallback(res) {
             $scope.images = res.data;
             $scope.image = $scope.images[0];
         }, function errorCallback(res) {
             $scope.images = [];
         });
-    $scope.setImage = function(imageId){
-        // /images?imageId=5830f1e57467e640eb4da00f
-        $http.get('/images', {param: {imageId: imageId}})
+}]);
+
+app.controller('pageController', ['$scope', '$http', function ($scope, $http) {
+    // /page
+    $scope.setPage = function (pageName) {
+        $http.get('/page', {param: {pageName: pageName}})
             .then(function successCallback(res) {
-                $scope.image = res.data;
+                $scope.page = res.data;
             }, function errorCallback(res) {
-                $scope.image = {};
+                $scope.page = {};
             });
-        }
-    }
-]);
+    };
+}]);
