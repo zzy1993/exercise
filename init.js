f5ce7ef62475257483c3d924911bb753ba09a119
zzy1993 <zzy1993@163.com>
2016-11-26 02:54:50 -0500

    not preserve page model

--- init.js
+++ init.js
@@ -6,45 +6,31 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/exercise');
 
 require('./app/models/imageModel.js');
-require('./app/models/pageModel.js');
 require('./app/models/commentModel.js');
+
 var Image = mongoose.model('Image');
-var Page = mongoose.model('Page');
 var Comment = mongoose.model('Comment');
 
 function addImage(title, filename) {
-    var image = new Image({title: title, filename: filename});
-    image.save(function () {
-        var comment = new Comment({title: title + " Comments"});
+    // BUG: sequence of save
+    var comment = new Comment({title: title + " Comments"});
+    comment.save(function (err, image) {
+        var image = new Image({title: title, filename: filename});
         image.commentId = comment._id;
+        // BUG: images.save()
         image.save(function () {
+            console.log(comment);
+            console.log(image);
             console.log(title + " is saved.");
         });
     });
 }
 
 Comment.remove().exec(function () {
-    Page.remove().exec(function () {
-        Image.remove().exec(function () {
-            var comment = new Comment({title: title});
-            comment.save(function (err, comment) {
-                var page = new Page({name: 'Images Page'});
-                page.commentId = comment.id;
-                page.save();
-            });
-            addImage('Boston_1728_Burgis', 'Boston_1728_Burgis.jpg');
-            addImage('Boston_1769_Price_LC', 'Boston_1769_Price_LC.jpg');
-            addImage('Boston_1775_deCosta', 'Boston_1775_deCosta.jpg');
-            addImage('Boston_1775_Harbor_Wheeler', 'Boston_1775_Harbor_Wheeler.jpg');
-            addImage('Boston_1775_Page', 'Boston_1775_Page.jpg');
-            addImage('Boston_1776_unknown', 'Boston_1776_unknown.jpg');
-            addImage('Boston_1777_Page', 'Boston_1777_Page.jpg');
-            addImage('Boston_1805_Carelton', 'Boston_1805_Carelton.jpg');
-            addImage('Boston_1814_Hales', 'Boston_1814_Hales.jpg');
-            addImage('Boston_1846_Smith', 'Boston_1846_Smith.jpg');
-            addImage('Boston_1852_Shields', 'Boston_1852_Shields.jpg');
-            addImage('Boston_1874_Hopkins', 'Boston_1874_Hopkins.jpg');
-            addImage('Boston_1875_Beers', 'Boston_1875_Beers.jpg');
-        });
+    Image.remove().exec(function () {
+        addImage('Boston_1728_Burgis', 'Boston_1728_Burgis.jpg');
+        addImage('Boston_1769_Price_LC', 'Boston_1769_Price_LC.jpg');
+        addImage('Boston_1775_deCosta', 'Boston_1775_deCosta.jpg');
+        addImage('Boston_1775_Harbor_Wheeler', 'Boston_1775_Harbor_Wheeler.jpg');
     });
 });
