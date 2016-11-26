7ce1a87bd99317fa73d2a4c0a4f5eeed533fe90d
zzy1993 <zzy1993@163.com>
2016-11-26 02:54:12 -0500

    remove page
    remove page

--- routes.js
+++ routes.js
@@ -3,33 +3,31 @@
  */
 
 var express = require('express');
-var bodyParser = require('body-parser');
 
 module.exports = function (app) {
-
-    // import controller modules
-    var image = require('./app/controllers/imageController.js');
-    var page = require('./app/controllers/pageController.js');
 
     // accessible files
     app.use('/', express.static(__dirname + '/app/views'));
     app.use('/static', express.static(__dirname + '/app/static'));
     app.use('/imgs', express.static(__dirname + '/imgs'));
     app.use('/lib', express.static(__dirname + '/lib'));
-    app.use(bodyParser());
 
     // response with template file
     app.get('/', function (req, res) {
         res.render('index.html');
     });
 
+    // import controller modules
+    var image = require('./app/controllers/imageController.js');
+    var comment = require('./app/controllers/commentController.js');
+
     // define request route to methods in controllers
     app.get('/image', image.getImage);
     app.get('/images', image.getImages);
-    app.get('/page', page.getPage);
+    app.get('/comment/get', comment.getComment);
+    app.post('/comment/post', comment.addComment);
     app.use('*', function (req, res) {
-        console.log('404');
-        res.send('404');
+        res.send('Content Not Found.', 404);
     });
 };
 
