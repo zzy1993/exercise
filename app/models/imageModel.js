b191d29670bfd0abdd22067bcb253e0b1c819cfb
zzy1993 <zzy1993@163.com>
2016-11-27 00:27:23 -0500

    wrongly deleted have to recover

---
+++ app/models/imageModel.js
@@ -0,0 +1,25 @@
+/**
+ * define image model:
+ * - title
+ * - filename
+ * - timestamp
+ * -- type
+ * -- default
+ * - commentId  (associate with a comment model
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// design schema of imgs table
+var ImageSchema = new Schema({
+    timestamp: {type: Date, default: Date.now},
+    title: String,
+    filename: String,
+    commentId: Schema.ObjectId
+},{
+    _id: true
+});
+
+// add mongoose model
+mongoose.model('Image', ImageSchema);
